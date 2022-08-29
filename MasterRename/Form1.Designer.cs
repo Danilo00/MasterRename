4351ddca2721d992df9866998ff2854537cd6033
Danilo Cappabianca <Danilo.Cappabianca@fincadsrl.local>
2022-08-29 12:21:23 +0200

    Aggiungere i file di progetto.

---
+++ MasterRename/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace MasterRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRename = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiChooseFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExtensionsMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ChoosedefaultExt = new System.Windows.Forms.ToolStripMenuItem();
+            this.ComboBoxExt = new System.Windows.Forms.ToolStripComboBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(148, 116);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(153, 39);
+            this.btnRename.TabIndex = 1;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(15, 59);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(283, 22);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "New File Name";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 87);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(286, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFolder,
+            this.ExtensionsMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(310, 30);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFolder
+            // 
+            this.tsmiFolder.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiChooseFolder});
+            this.tsmiFolder.Name = "tsmiFolder";
+            this.tsmiFolder.Size = new System.Drawing.Size(72, 26);
+            this.tsmiFolder.Text = "Folder: ";
+            // 
+            // tsmiChooseFolder
+            // 
+            this.tsmiChooseFolder.Name = "tsmiChooseFolder";
+            this.tsmiChooseFolder.Size = new System.Drawing.Size(224, 26);
+            this.tsmiChooseFolder.Text = "Choose Folder";
+            this.tsmiChooseFolder.Click += new System.EventHandler(this.tsmiChooseFolder_Click);
+            // 
+            // ExtensionsMenu
+            // 
+            this.ExtensionsMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ChoosedefaultExt,
+            this.ComboBoxExt});
+            this.ExtensionsMenu.Name = "ExtensionsMenu";
+            this.ExtensionsMenu.Size = new System.Drawing.Size(92, 24);
+            this.ExtensionsMenu.Text = "Extensions";
+            // 
+            // ChoosedefaultExt
+            // 
+            this.ChoosedefaultExt.Checked = true;
+            this.ChoosedefaultExt.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ChoosedefaultExt.Name = "ChoosedefaultExt";
+            this.ChoosedefaultExt.Size = new System.Drawing.Size(195, 26);
+            this.ChoosedefaultExt.Text = "Default";
+            this.ChoosedefaultExt.Click += new System.EventHandler(this.ChoosedefaultExt_Click);
+            // 
+            // ComboBoxExt
+            // 
+            this.ComboBoxExt.Name = "ComboBoxExt";
+            this.ComboBoxExt.Size = new System.Drawing.Size(121, 28);
+            this.ComboBoxExt.TextChanged += new System.EventHandler(this.ComboBoxExt_ChangeText);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(72, 127);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            this.statusLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(310, 167);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Master Rename";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ExtensionsMenu;
+        private System.Windows.Forms.ToolStripMenuItem ChoosedefaultExt;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFolder;
+        private System.Windows.Forms.ToolStripComboBox ComboBoxExt;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ToolStripMenuItem tsmiChooseFolder;
+    }
+}
+
---
+++ MasterRename/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MasterRename
+{
+    public partial class Form1 : Form
+    {
+        string FolderPath = string.Empty;
+        string ext = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            //Main Code            
+            if (FolderPath != string.Empty)
+            {
+                string[] files = Directory.GetFiles(FolderPath);
+
+                statusLabel.Text = string.Empty;
+                progressBar.Value = 0;
+                progressBar.Maximum = files.Length;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = new FileInfo(files[i]);
+                    ext = ExtSelector(file);
+                    //If you want to create some new rules, put your code here or edit this row
+                    string NewFileName = txtFileName.Text + i.ToString() + ext;
+                    File.Move(files[i], files[i].Replace(file.Name, NewFileName));
+                    progressBar.Value++;
+                    statusLabel.Text = "Loading...";
+                }
+                statusLabel.Text = "Done.";
+            }
+            else MessageBox.Show("Please select a Folder.");
+        }
+        private void tsmiChooseFolder_Click(object sender, EventArgs e)   //Open a dialog Folder Browser and choose Folder with Files
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    FolderPath = fbd.SelectedPath;
+                    tsmiFolder.Text = tsmiFolder.Text + Path.GetFileName(FolderPath);
+
+                    //Check empty folder
+                    string[] files = Directory.GetFiles(FolderPath);
+                    if (files.Length == 0) MessageBox.Show("Please Choose a Folder with Files to Rename", "Folder not valid");
+                }
+            }            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Pre Load Extensions
+            ComboBoxExt.Items.Add(".jpg");
+            ComboBoxExt.Items.Add(".jpeg");
+            ComboBoxExt.Items.Add(".png");
+            //ExtensionsMenu.Visible = false; //You can use this code if you want to disable the use of any custom extension
+        }
+        private string ExtSelector(FileInfo file)   //Select the value of the chosen extension string
+        {   
+            string CustomExt = ComboBoxExt.Text;
+            if (!ComboBoxExt.Text.StartsWith(".")) CustomExt = Path.Combine(".", CustomExt);
+
+            if (ChoosedefaultExt.Checked) ext = file.Extension;
+            else ext = CustomExt;
+            return ext;
+        }
+
+        private void ComboBoxExt_ChangeText(object sender, EventArgs e)   //Enable/disable the default extension to the change of custom extension textbox
+        {
+            if (ComboBoxExt.Text != string.Empty) ChoosedefaultExt.Checked = false;
+            else ChoosedefaultExt.Checked = true;
+        }
+
+        private void ChoosedefaultExt_Click(object sender, EventArgs e)   //Enable Default extension on click if disabled
+        {
+            if (!ChoosedefaultExt.Checked) ChoosedefaultExt.Checked = true;
+        }
+
+    }
+}
